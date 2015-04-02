2839cf41365f4402ed85e55854eb23bda439a5c3
filipok <filip.gadiuta@gmail.com>
2015-04-02 13:30:48 +0300

    add constants

---
+++ align/const.py
@@ -0,0 +1,10 @@
+# Name:        const.py
+# Purpose:     Constants
+#
+# Author:      Filip
+#
+# Created:     2.4.2015
+
+ALL_LANGS = ['bg', 'es', 'cs', 'da', 'de', 'et', 'el', 'en', 'fr', 'ga', 'hr',
+             'it', 'lv', 'lt', 'hu', 'mt', 'nl', 'pl', 'pt', 'ro', 'sk', 'sl',
+             'fi', 'sv']
