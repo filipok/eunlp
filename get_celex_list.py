265598d28f2651f9b24938f9eba5b154d6d88f0e
Filip <filip.gadiuta@gmail.com>
2015-04-04 07:43:49 +0300

    test for argument validity and add option to perform all possible alignments

--- get_celex_list.py
+++ get_celex_list.py
@@ -13,6 +13,7 @@ import os
 import logging
 
 from align import align, convert
+from align.const import ALL_LANGS
 
 
 logging.basicConfig(filename='log.txt', level=logging.WARNING)
@@ -23,13 +24,33 @@ logging.getLogger('').addHandler(console)
 if __name__ == '__main__':
     # collect arguments
     path = os.getcwd()
-    program_folder = os.path.dirname(sys.argv[0])
-    if len(program_folder) != 0:
-        program_folder += '/'
-    xml_list = sys.argv[1]  # collect xml list name
-    languages = sys.argv[2:]  # collect language codes
+    if len(sys.argv) > 2:
+        program_folder = os.path.dirname(sys.argv[0])
+        if len(program_folder) != 0:
+            program_folder += '/'
+        xml_list = sys.argv[1]  # collect xml list name
+        langs = [x.lower() for x in sys.argv[2:]]  # collect language codes
+        if (len(langs) == 2 and len(set(langs)) == 2 and
+                set(langs).issubset(set(ALL_LANGS))):
+            file_list = convert.eu_xml_converter(xml_list)
+            for item in file_list:
+                print "Processing " + item[0] + ' ...'
+                align.celex_aligner(langs, path, item[0], '', program_folder)
+        elif len(langs) == 1 and langs[0] == 'all':
+            file_list = convert.eu_xml_converter(xml_list)
+            for i in range(len(ALL_LANGS)):
+                languages = ALL_LANGS[:]
+                s_lang = languages.pop(i)
+                for t_lang in languages:
+                    for item in file_list:
+                        print "Processing pair {}-{}, document {}".format(
+                            s_lang, t_lang, item[0])
+                        align.celex_aligner([s_lang, t_lang], path, item[0],
+                                            s_lang + t_lang, program_folder)
 
-    file_list = convert.eu_xml_converter(xml_list)
-    for item in file_list:
-        print "Processing " + item[0] + ' ...'
-        align.celex_aligner(languages, path, item[0], '', program_folder)
+        else:
+            logging.critical('Invalid languages!')
+            print "Either pass 'ALL' or a pair of valid languages."
+            print "Valid languages:", ALL_LANGS
+    else:
+        logging.critical('Too few parameters!')
