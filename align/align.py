b68fac7704d73690d346047826c7c72ec88457ab
Filip <filip.gadiuta@gmail.com>
2015-05-20 19:19:28 +0300

    add negative lookahead for Hungarian in pattern 1 and restrict pattern 3 to max 3 numbers

--- align/align.py
+++ align/align.py
@@ -29,14 +29,12 @@ def paragraph_combiner_sub(text):
     :rtype: str
     """
     # pattern 1 combines 1-3 letters/numbers with dot/brackets with next line
-    # pattern_1 = re.compile(
-    #     r'\n\(?([0-9]{1,3}|[a-z]{1,3}|[A-Z]{1,3})[\.\)][\n\s]')
-    pattern_1_unicode = re.compile(r'\n\(?(\w{1,3})[\.\)][\n\s]', re.UNICODE)
-    # pattern 3 combines single number + single letter with the next line
-    # pattern_3 = re.compile(r'\n\(?([0-9]+[a-z]+)[\.\)][\n\s]')
-    # TODO asta zapaceste EN-HR 32014D0372 si 32014D0190, \w poa sa fie cifra
-    # TODO in HU am "2. cikk" (Article 2) si trunchiaza
-    pattern_3_unicode = re.compile(r'\n\(?([0-9]+\w+)[\.\)][\n\s]', re.UNICODE)
+    # the negative lookahead (?!cikk) is for Hungarian.
+    pattern_1_unicode = re.compile(r'\n\(?(\w{1,3})[\.\)][\n\s](?!cikk)',
+                                   re.UNICODE)
+    # pattern 3 combines 1-3 numbers + single letter with the next line
+    pattern_3_unicode = re.compile(
+        r'\n\(?([0-9]{1,3}(?![0-9])\w+)[\.\)][\n\s]', re.UNICODE)
     # combine lines consisting of Roman numerals to 9 with the next line
     pattern_4 = re.compile(r'\n\(?(i{1,3})[\.\)][\n\s]')  # 1-3
     pattern_5 = re.compile(r'\n\(?(iv)[\.\)][\n|\s]')  # 4
