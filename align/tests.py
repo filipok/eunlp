32f1a6b9ba2ab3256fd9a7ca28c718527e11942f
Filip <filip.gadiuta@gmail.com>
2015-12-23 23:47:42 +0200

    more tests

--- align/tests.py
+++ align/tests.py
@@ -38,6 +38,96 @@ class TestConvert(unittest.TestCase):
                                  '20151223T190423Z', '32013R1024',
                                  '<prop type="Txt::Alignment">Hun</prop>'))
 
+    def test_tab_line_hun(self):
+        s_lang = 'en'
+        t_lang = 'ro'
+        now = '20151223T190423Z'
+        note = '32013R1024'
+        line ='Hun\tAcesta e un rand.\tThis is a line.\n'
+        resu = ('<tu creationdate="20151223T190423Z" creationid="eunlp">'
+                '<prop type="Txt::Note">32013R1024</prop>'
+                '<prop type="Txt::Alignment">Hun</prop>\n'
+                '<tuv xml:lang="en"><seg>This is a line.</seg></tuv>\n'
+                '<tuv xml:lang="ro"><seg>Acesta e un rand.</seg></tuv> </tu>\n'
+                '\n')
+        self.assertEqual(resu, convert.tab_line(line, s_lang, t_lang, now,
+                                                note))
+
+    def test_tab_line_nai(self):
+        s_lang = 'en'
+        t_lang = 'ro'
+        now = '20151223T190423Z'
+        note = '32013R1024'
+        line ='Nai\tAcesta e un rand.\tThis is a line.\n'
+        resu = ('<tu creationdate="20151223T190423Z" creationid="eunlp">'
+                '<prop type="Txt::Note">32013R1024</prop>'
+                '<prop type="Txt::Alignment">Short</prop>\n'
+                '<tuv xml:lang="en"><seg>This is a line.</seg></tuv>\n'
+                '<tuv xml:lang="ro"><seg>Acesta e un rand.</seg></tuv> </tu>\n'
+                '\n')
+        self.assertEqual(resu, convert.tab_line(line, s_lang, t_lang, now,
+                                                note))
+
+    def test_tab_line_err(self):
+        s_lang = 'en'
+        t_lang = 'ro'
+        now = '20151223T190423Z'
+        note = '32013R1024'
+        line ='Err\tAcesta e un rand.\tThis is a line.\n'
+        resu = ('<tu creationdate="20151223T190423Z" creationid="eunlp">'
+                '<prop type="Txt::Note">32013R1024</prop>'
+                '<prop type="Txt::Alignment">Long_f</prop>\n'
+                '<tuv xml:lang="en"><seg>This is a line.</seg></tuv>\n'
+                '<tuv xml:lang="ro"><seg>Acesta e un rand.</seg></tuv> </tu>\n'
+                '\n')
+        self.assertEqual(resu, convert.tab_line(line, s_lang, t_lang, now,
+                                                note))
+
+    def test_tab_line_other(self):
+        s_lang = 'en'
+        t_lang = 'ro'
+        now = '20151223T190423Z'
+        note = '32013R1024'
+        line ='Blabla\tAcesta e un rand.\tThis is a line.\n'
+        resu = ('<tu creationdate="20151223T190423Z" creationid="eunlp">'
+                '<prop type="Txt::Note">32013R1024</prop>'
+                '<prop type="Txt::Alignment">Unknown</prop>\n'
+                '<tuv xml:lang="en"><seg>This is a line.</seg></tuv>\n'
+                '<tuv xml:lang="ro"><seg>Acesta e un rand.</seg></tuv> </tu>\n'
+                '\n')
+        self.assertEqual(resu, convert.tab_line(line, s_lang, t_lang, now,
+                                                note))
+
+    def test_tab_line_other_tildas(self):
+        s_lang = 'en'
+        t_lang = 'ro'
+        now = '20151223T190423Z'
+        note = '32013R1024'
+        line ='Blabla\t~~~ Acesta e un rand.\t~~~ This is a line.\n'
+        resu = ('<tu creationdate="20151223T190423Z" creationid="eunlp">'
+                '<prop type="Txt::Note">32013R1024</prop>'
+                '<prop type="Txt::Alignment">Unknown</prop>\n'
+                '<tuv xml:lang="en"><seg>This is a line.</seg></tuv>\n'
+                '<tuv xml:lang="ro"><seg>Acesta e un rand.</seg></tuv> </tu>\n'
+                '\n')
+        self.assertEqual(resu, convert.tab_line(line, s_lang, t_lang, now,
+                                                note))
+
+    def test_tab_line_other_xml_escapes(self):
+        s_lang = 'en'
+        t_lang = 'ro'
+        now = '20151223T190423Z'
+        note = '32013R1024'
+        line ='Blabla\t& < > e un rand.\t& < > is a line.\n'
+        resu = ('<tu creationdate="20151223T190423Z" creationid="eunlp">'
+                '<prop type="Txt::Note">32013R1024</prop>'
+                '<prop type="Txt::Alignment">Unknown</prop>\n'
+                '<tuv xml:lang="en"><seg>&amp; &lt; &gt; is a line.</seg></tuv>\n'
+                '<tuv xml:lang="ro"><seg>&amp; &lt; &gt; e un rand.</seg></tuv> </tu>\n'
+                '\n')
+        self.assertEqual(resu, convert.tab_line(line, s_lang, t_lang, now,
+                                                note))
+
 
 if __name__ == '__main__':
     unittest.main()
