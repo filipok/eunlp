c12331cf9e3aa82cf54622172a775bac25a0003a
Filip <filip.gadiuta@gmail.com>
2015-12-24 00:57:26 +0200

    even more tests

--- align/tests.py
+++ align/tests.py
@@ -2,6 +2,7 @@ __author__ = 'filip'
 
 import unittest
 import convert
+import re
 
 
 class TestConvert(unittest.TestCase):
@@ -128,6 +129,67 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(resu, convert.tab_line(line, s_lang, t_lang, now,
                                                 note))
 
+    def test_tab_to_tmx(self):
+        resu = ('<?xml version="1.0" encoding="utf-8" ?>\n'
+                '<!DOCTYPE tmx SYSTEM "tmx14.dtd">\n'
+                '<tmx version="1.4">\n'
+                '  <header\n'
+                '    creationtool="eunlp"\n'
+                '    creationtoolversion="0.01"\n'
+                '    datatype="unknown"\n'
+                '    segtype="sentence"\n'
+                '    adminlang="en"\n'
+                '    srclang="en"\n'
+                '    o-tmf="TW4Win 2.0 Format"\n'
+                '  >\n'
+                '  </header>\n'
+                '  <body>\n'
+                '<tu creationdate="20151224T000530Z" creationid="eunlp">'
+                '<prop type="Txt::Note">32013R1024</prop>'
+                '<prop type="Txt::Alignment">Long_f</prop>\n'
+                '<tuv xml:lang="en"><seg>This is a line.</seg></tuv>\n'
+                '<tuv xml:lang="ro"><seg>Acesta e un rand.</seg></tuv> </tu>\n'
+                '\n'
+                '<tu creationdate="20151224T000530Z" creationid="eunlp">'
+                '<prop type="Txt::Note">32013R1024</prop>'
+                '<prop type="Txt::Alignment">Hun</prop>\n'
+                '<tuv xml:lang="en"><seg>This is a line.</seg></tuv>\n'
+                '<tuv xml:lang="ro"><seg>Acesta e un rand.</seg></tuv> </tu>\n'
+                '\n'
+                '<tu creationdate="20151224T000530Z" creationid="eunlp">'
+                '<prop type="Txt::Note">32013R1024</prop>'
+                '<prop type="Txt::Alignment">Short</prop>\n'
+                '<tuv xml:lang="en"><seg>This is a line.</seg></tuv>\n'
+                '<tuv xml:lang="ro"><seg>Acesta e un rand.</seg></tuv> </tu>\n'
+                '\n'
+                '\n'
+                '</body>\n'
+                '</tmx>')
+        tab_file = ('Err\tAcesta e un rand.\tThis is a line.\n'
+                    'Hun\tAcesta e un rand.\tThis is a line.\n'
+                    'Nai\tAcesta e un rand.\tThis is a line.\n')
+        s_lang = 'en'
+        t_lang = 'ro'
+        note = '32013R1024'
+        actual_resu = convert.tab_to_tmx(tab_file, s_lang, t_lang, note)
+        resu = re.sub(r'([0-9]){8}T([0-9]){6}Z', r'00000000T000000Z', resu)
+        actual_resu = re.sub(r'([0-9]){8}T([0-9]){6}Z', r'00000000T000000Z',
+                             actual_resu)
+        self.assertEqual(resu, actual_resu)
+
+    def test_tab_to_separate(self):
+        tab_file = ('Err\tAcesta e un rand.\tThis is a line.\n'
+                    'Hun\tAcesta e un rand.\tThis is a line.\n'
+                    'Nai\tAcesta e un rand.\tThis is a line.\n')
+        s_list = ['This is a line.', 'This is a line.', 'This is a line.']
+        t_list = ['Acesta e un rand.', 'Acesta e un rand.',
+                  'Acesta e un rand.']
+        self.assertEqual((s_list, t_list), convert.tab_to_separate(tab_file))
+
+    def test_split_line(self):
+        line = 'Err\tAcesta e un rand.\tThis is a line.\n'
+        self.assertEqual(('This is a line.', 'Acesta e un rand.'),
+                         convert.split_line(line))
 
 if __name__ == '__main__':
     unittest.main()
