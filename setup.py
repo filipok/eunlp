403a6e13a0ef5f10508f217ced5e95cd5f151f74
Filip <filip.gadiuta@gmail.com>
2016-03-01 19:49:08 +0200

    increment align and correct changelod

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='align',
-    version='0.4.4',
+    version='0.4.5',
     packages=['align'],
     include_package_data=True,
     description='Align EU legislation.',
